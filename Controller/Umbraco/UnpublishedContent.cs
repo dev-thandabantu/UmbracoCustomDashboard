330a6703c1f55688888bd4c1f5ad10d08e4011bb
Brighton Tandabantu <brighton.tandabantu@fullstack.co.za>
2022-08-15 10:00:49 +0200

    Add project files.

---
+++ Controller/Umbraco/UnpublishedContent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Web.Common.Authorization;
+using Umbraco.Cms.Web.Common.Filters;
+
+namespace DemoUmbraco.Controller.Umbraco
+{
+    [Authorize(Policy = AuthorizationPolicies.BackOfficeAccess)]
+    [DisableBrowserCache]
+    // https://github.com/umbraco/Umbraco-CMS/blob/v9/contrib/src/Umbraco.Web.Common/Controllers/UmbracoAuthorizedController.cs
+    public class AdminController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public AdminController(
+            //ISearchService searchService
+            )
+        {
+            //_searchService = searchService;
+        }
+
+        public IActionResult Index()
+        {
+            return View("UnpublishedContent");
+        }
+    }
+}
